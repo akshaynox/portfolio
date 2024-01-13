6fecbf2c11889948e1dd588c20c1a4e2876dab98
Akshay <aksingh240@gmail.com>
2024-01-14 03:00:51 +0530

    added about section

---
+++ components/about.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { motion } from "framer-motion";
+import SectionHeading from "./section-heading";
+
+const About = () => {
+  return (
+    <motion.section
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      id="about"
+    >
+      <SectionHeading>About me</SectionHeading>
+      <p className="mb-3">
+        After graduating with a degree in{" "}
+        <span className="font-medium">Computer Science</span>, I started working
+        as a software engineer and got into{" "}
+        <span className="font-medium">full-stack web development</span>.{" "}
+        <span className="italic">My favorite part of programming</span> is the
+        problem-solving aspect. I <span className="underline">love</span> the
+        feeling of finally figuring out a solution to a problem. My core stack
+        is{" "}
+        <span className="font-medium">
+          React, Next.js, Node.js, and TypeScript
+        </span>
+        . I am also familiar with MongoDB, Postgres, Redis & Tailwind. I am
+        always looking to learn new technologies.
+        <p>
+          <span className="italic">When I'm not coding</span>, I enjoy playing
+          video games, watching movies, and reading sci-fi/fantasy books.
+        </p>
+      </p>
+    </motion.section>
+  );
+};
+
+export default About;
