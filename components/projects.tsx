1994f7e116e00469db990094d2f9cb7bb18f7a51
Akshay <mail.akshayks@gmail.com>
2024-01-21 00:20:04 +0530

    added section refs for header animation

--- components/projects.tsx
+++ components/projects.tsx
@@ -5,6 +5,7 @@ import SectionHeading from "./section-heading";
 import { projectsData } from "@/lib/data";
 import Image from "next/image";
 import { motion, useScroll, useTransform } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
 
 type ProjectProps = (typeof projectsData)[number];
 
@@ -79,8 +80,10 @@ const Project: React.FC<ProjectProps> = ({
 };
 
 const Projects = () => {
+  const { ref } = useSectionInView("Projects", 0.5);
+
   return (
-    <section id="projects" className="scroll-mt-28 mb-28">
+    <section id="projects" className="scroll-mt-28 mb-28" ref={ref}>
       <SectionHeading>My Projects</SectionHeading>
       <div>
         {projectsData.map((project, index) => (
