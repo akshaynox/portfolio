497c0007f8e5f1283c252567176fddd9edf10e99
Akshay <mail.akshayks@gmail.com>
2024-01-30 01:46:02 +0530

    fixed socials icons alignment

--- components/intro.tsx
+++ components/intro.tsx
@@ -101,25 +101,26 @@ const Intro = () => {
           <HiDownload className="opacity-60 group-hover:translate-y-0 transition" />
         </a>
 
-        <a
-          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center 
+        <div className="flex gap-2">
+          <a
+            className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center 
             gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition 
             cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://linkedin.com/in/akshay240"
-          target="_blank"
-        >
-          <BsLinkedin />
-        </a>
-
-        <a
-          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] 
+            href="https://linkedin.com/in/akshay240"
+            target="_blank"
+          >
+            <BsLinkedin />
+          </a>
+          <a
+            className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] 
             rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 
             transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com/akshaynox"
-          target="_blank"
-        >
-          <FaGithubSquare />
-        </a>
+            href="https://github.com/akshaynox"
+            target="_blank"
+          >
+            <FaGithubSquare />
+          </a>
+        </div>
       </motion.div>
     </section>
   );
